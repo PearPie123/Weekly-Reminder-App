1561ad6fb7172be07af4b065dab1f1f826aff6c2
PearPie <aroberts24@mylcusd.net>
2021-06-25 12:53:08 -0700

    Added a little css for the tab selector buttons.

--- script.mjs
+++ script.mjs
@@ -1,4 +1,4 @@
-const tabSelectorBtns = document.getElementsByClassName("tabSelectBtn");
+const tabSelectorBtns = document.getElementsByClassName("tabSelectorBtn");
 const btnToTabKey = {
   "todoTabBtn": "todoTab",
   "upcomingTabBtn": "upcomingTab",
@@ -8,14 +8,20 @@ for(const btn of tabSelectorBtns) {
   const tabs = document.getElementsByClassName("tab");
   const correspondingTab = document.getElementById(btnToTabKey[btn.id]);
   btn.onclick = () => {
-    selectTab(tabs, correspondingTab);
+    selectTab(tabs, tabSelectorBtns, correspondingTab, btn);
   }
 }
-const selectTab = (tabList, tab) => {
+const selectTab = (tabList, btnList, tab, btn) => {
   for(const tabElem of tabList) {
     tabElem.classList.remove("activeTab");
     tabElem.classList.add("inactiveTab");
   }
   tab.classList.add("activeTab");
   tab.classList.remove("inactiveTab");
+  for(const btnElem of btnList) {
+    btnElem.classList.remove("activeTabBtn");
+    btnElem.classList.add("inactiveTabBtn");
+  }
+  btn.classList.add("activeTabBtn");
+  btn.classList.remove("inactiveTabBtn");
 }
