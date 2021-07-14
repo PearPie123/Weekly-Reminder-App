b6bd5f63a0fb651f159277528dd313431c064cf7
PearPie <aroberts24@mylcusd.net>
2021-07-14 14:14:21 -0700

    simplified the previous code by a lot

--- script.mjs
+++ script.mjs
@@ -25,27 +25,17 @@ const selectTab = (tabList, btnList, tab, btn) => {
   btn.classList.add("activeTabBtn");
   btn.classList.remove("inactiveTabBtn");
 }
-// can be daily monthly or weekly
-// has a date var that is when it should trigger
-// can be set to happen again
+class ReoccuringReminder {
+  constructor() {
+    
+  }
+}
 class Reminder {
   constructor(triggerDate) {
     this.triggerDate = triggerDate;
-    this.isUpcoming(this.triggerDate);
-    const interval = setInterval(this.isUpcoming, 60000, this.triggerDate);
-  }
-
-  isUpcoming(triggerDate) {
-    const now = Date.now();
-    const milisecDifference = triggerDate.getTime() - now;
-    if(milisecDifference <= 300000) {
-      this.prepareTrigger(milisecDifference);
-      return true;
-    }
-  }
-
-  prepareTrigger(timeTillTrigger) {
-    setTimeout(this.triggerReminder, timeTillTrigger);
+    this.creationDate = new Date();
+    const milisecDifference = triggerDate.getTime() - this.creationDate.getTime();
+    setTimeout(this.triggerReminder, milisecDifference);
   }
 
   triggerReminder() {
@@ -53,3 +43,31 @@ class Reminder {
   }
 
 }
+// can be daily monthly or weekly
+// has a date var that is when it should trigger
+// can be set to happen again
+// class Reminder {
+//   constructor(triggerDate) {
+//     this.triggerDate = triggerDate;
+//     this.isUpcoming(this.triggerDate);
+//     const interval = setInterval(this.isUpcoming, 60000, this.triggerDate);
+//   }
+
+//   isUpcoming(triggerDate) {
+//     const now = Date.now();
+//     const milisecDifference = triggerDate.getTime() - now;
+//     if(milisecDifference <= 300000) {
+//       this.prepareTrigger(milisecDifference);
+//       return true;
+//     }
+//   }
+
+//   prepareTrigger(timeTillTrigger) {
+//     setTimeout(this.triggerReminder, timeTillTrigger);
+//   }
+
+//   triggerReminder() {
+//     alert("Reminder Triggered!!!!")
+//   }
+
+// }
